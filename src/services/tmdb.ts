362f16d7f53236f32e10907c787840ee534c96df
Rahmat Suhadi <rahmatsuhadi32@gmail.com>
2026-06-09 18:37:45 +0700

    feat: implement getPopularMoviesClient and searchMoviesClient functions

---
+++ src/services/tmdb.ts
@@ -0,0 +1,17 @@
+import { MovieResponse } from "@/types";
+
+
+export async function getPopularMoviesClient(page = 1): Promise<MovieResponse> {
+    const res = await fetch(`/api/movies/popular?page=${page}`);
+    if (!res.ok) throw new Error("ERROR: failed get data popular");
+    return res.json();
+}
+
+export async function searchMoviesClient(query: string, page = 1): Promise<MovieResponse> {
+    if (!query.trim()) {
+      return { page: 1, results: [], total_pages: 1, total_results: 0 };
+    }
+    const res = await fetch(`/api/movies/search?query=${encodeURIComponent(query)}&page=${page}`);
+    if (!res.ok) throw new Error("ERROR: failed search movie");
+    return res.json();
+  }
