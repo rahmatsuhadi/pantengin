90b346f0298247c26b44fbbcdacb204d71875071
Rahmat Suhadi <rahmatsuhadi32@gmail.com>
2026-06-09 19:28:24 +0700

    feat: configure image settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  cacheComponents: true,
+  images: {
+    qualities: [70, 75, 50],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
