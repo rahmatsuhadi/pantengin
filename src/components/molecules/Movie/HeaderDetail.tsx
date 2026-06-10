1c1f72e4d0877f4f58ebe2073add56a7937805c7
Rahmat Suhadi <rahmatsuhadi32@gmail.com>
2026-06-10 16:09:49 +0700

    feat: add back navigation functionality to HeaderDetailMovie component

--- src/components/molecules/Movie/HeaderDetail.tsx
+++ src/components/molecules/Movie/HeaderDetail.tsx
@@ -1,19 +1,21 @@
+"use client"
 import { Button } from "@/components/atoms/Button";
 import BrandLogo from "@/components/atoms/Logo";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function HeaderDetailMovie() {
-    return (
+    const router = useRouter()
 
+    return (
         <header className="fixed top-0 left-0 right-0 z-50">
             <div className="glass">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex items-center justify-between h-16">
                         <BrandLogo />
                             <Button 
-                            as={Link}
-                             href="/"
+                            onClick={() => router.back()}
                              size="sm"
                              variant="ghost"
                             >
